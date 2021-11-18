25e59c52a9eb48139b183570d6b9acba9d800946
Poga Huang <pgh86812@gmail.com>
2021-11-18 18:20:30 +0800

    Initial Commit

---
+++ Homework17UIBezierPath/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Homework17UIBezierPath
+//
+//  Created by 黃柏嘉 on 2021/11/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Homework17UIBezierPath/Homework17UIBezierPathApp.swift
@@ -0,0 +1,17 @@
+//
+//  Homework17UIBezierPathApp.swift
+//  Homework17UIBezierPath
+//
+//  Created by 黃柏嘉 on 2021/11/18.
+//
+
+import SwiftUI
+
+@main
+struct Homework17UIBezierPathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
